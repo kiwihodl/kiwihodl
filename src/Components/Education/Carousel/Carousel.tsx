4814693a3c0234f0b77dc6274c7a5eb8a65c9510
kiwihodl <orangeamber@protonmail.com>
2024-01-18 17:54:17 -0600

    fixing chevrons for content change and making responsive

--- src/Components/Education/Carousel/Carousel.tsx
+++ src/Components/Education/Carousel/Carousel.tsx
@@ -1,63 +1,81 @@
 import React, { useState } from "react";
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, useBreakpointValue } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import Card from "../ContentCards/CardStructure";
 import MockData from "../../../mock-data.json";
 
 const CardCarousel = () => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handlePrev = () => {
-        setIndex((old) => (old === 0 ? MockData.length - 1 : old - 1));
-    };
+  const handlePrev = () => {
+    setIndex((old) => (old === 0 ? MockData.length - 1 : old - 1));
+  };
 
-    const handleNext = () => {
-        setIndex((old) => (old === MockData.length - 1 ? 0 : old + 1));
-    };
+  const handleNext = () => {
+    setIndex((old) => (old === MockData.length - 1 ? 0 : old + 1));
+  };
 
-    const user = {
+  const user = {
     ...MockData[index],
-    Category: MockData[index].Category || '',
-    ThumbNailAltDescription: MockData[index].ThumbNailAltDescription || '',
+    Category: MockData[index].Category || "",
+    ThumbNailAltDescription: MockData[index].ThumbNailAltDescription || "",
     Watch: {
-        ...MockData[index].Watch,
-        Bitcoin_Platforms: {
-            ...MockData[index].Watch.Bitcoin_Platforms,
-            Spotify: '',
-            ApplePodcasts: ''
-        }
+      ...MockData[index].Watch,
+      Bitcoin_Platforms: {
+        ...MockData[index].Watch.Bitcoin_Platforms,
+        Spotify: "",
+        ApplePodcasts: "",
+      },
     },
     Listen: {
-        ...MockData[index].Listen,
-        Bitcoin_Platforms: {
-            ...MockData[index].Listen.Bitcoin_Platforms,
-            Spotify: '',
-            ApplePodcasts: ''
-        }
+      ...MockData[index].Listen,
+      Bitcoin_Platforms: {
+        ...MockData[index].Listen.Bitcoin_Platforms,
+        Spotify: "",
+        ApplePodcasts: "",
+      },
     },
     Read: {
-        ...MockData[index].Read,
-        Bitcoin_Platforms: {
-            ...MockData[index].Read.Bitcoin_Platforms,
-            Spotify: '',
-            ApplePodcasts: ''
-        }
-    }
-};
+      ...MockData[index].Read,
+      Bitcoin_Platforms: {
+        ...MockData[index].Read.Bitcoin_Platforms,
+        Spotify: "",
+        ApplePodcasts: "",
+      },
+    },
+  };
 
-    return (
-        <Flex width="full" align="center" justifyContent="center" mt={20}>
-            <Button onClick={handlePrev} leftIcon={<ChevronLeftIcon />} m={1}>
-                Prev
-            </Button>
-            <Box width="full" p={4} display="flex" justifyContent="center">
-                <Card user={user} />
-            </Box>
-            <Button onClick={handleNext} rightIcon={<ChevronRightIcon />} m={1}>
-                Next
-            </Button>
-        </Flex>
-    );
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  return (
+    <Flex width="full" align="center" justifyContent="center" mt={20}>
+      {!isMobile && (
+        <Button
+          onClick={handlePrev}
+          leftIcon={<ChevronLeftIcon boxSize={60} />}
+          m={1}
+          bg="transparent"
+          border="none"
+          color="#FF8700"
+          marginTop="400px"
+        ></Button>
+      )}
+      <Box width="full" p={4} display="flex" justifyContent="center">
+        <Card user={user} />
+      </Box>
+      {!isMobile && (
+        <Button
+          onClick={handleNext}
+          rightIcon={<ChevronRightIcon boxSize={60} />}
+          m={1}
+          bg="transparent"
+          border="none"
+          color="#FF8700"
+          marginTop="400px"
+        ></Button>
+      )}
+    </Flex>
+  );
 };
 
 export default CardCarousel;
