f46e655637bd8badd09c043b06044b63f7700c04
kiwihodl <orangeamber@protonmail.com>
2024-01-18 18:07:42 -0600

    fixing nav to be sticky on desktop only

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useBreakpointValue } from "@chakra-ui/react";
 import theme from "./Utils/theme";
 import Nav from "./Components/Navbar/Navbar"
 import CardCarousel from './Components/Education/Carousel/Carousel';
@@ -22,7 +22,7 @@ function App() {
     <ChakraProvider theme={theme}>
       <Box
         position="absolute"
-        top="13px"
+        top={{ base: "-55px", md: "13px" }}
         left="21px"
         width="60px"
         height="60px"
@@ -34,7 +34,7 @@ function App() {
 
       <Box
         position="absolute"
-        top="6px"
+        top={{ base: "-62px", md: "6px" }}
         left="14px"
         width="58px"
         height="58px"
