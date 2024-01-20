605fa05b98e6cb62bd7dc85dc724a7ba9c5bea71
kiwihodl <orangeamber@protonmail.com>
2024-01-20 12:11:35 -0600

    adding the + icon for entering new content into graphQL

--- src/Components/Education/Search/AddContent.tsx
+++ src/Components/Education/Search/AddContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Modal, ModalOverlay, ModalContent, ModalCloseButton, Icon } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 
 interface AddContentProps {
     trigger: boolean;
@@ -8,14 +9,20 @@ interface AddContentProps {
 }
 
 const AddContent: React.FC<AddContentProps> = (props) => {
-    return props.trigger ? (
-        <Box className='popup'>
-            <Box className="popup-inner">
-                <Button className='close-btn' onClick={() => props.setTrigger(false)}></Button>
-                {props.children}
-            </Box>
-        </Box>
-    ) : null;
+    return (
+        <>
+            <Button variant="unstyled" onClick={() => props.setTrigger(true)}>
+                <Icon as={AddIcon} color="#FF8700" boxSize="1.8em" />
+            </Button>
+            <Modal isOpen={props.trigger} onClose={() => props.setTrigger(false)}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalCloseButton />
+                    {props.children}
+                </ModalContent>
+            </Modal>
+        </>
+    );
 }
 
 export default AddContent;
--- src/Components/Education/Search/SearchEducation.tsx
+++ src/Components/Education/Search/SearchEducation.tsx
@@ -1,37 +1,41 @@
 import { Box, Button, Input, Flex, Select } from "@chakra-ui/react";
+import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import AddContent from "./AddContent"
 
-interface AddContentProps {
+interface SearchEducationProps {
   children?: React.ReactNode;
 }
 
-const AddContent: React.FC<AddContentProps> = ({ children }) => {
+const SearchEducation: React.FC<SearchEducationProps> = ({ children }) => {
+  const [trigger, setTrigger] = useState(false);
+
   return (
     <Box mt={10}>
       <Flex align="center" justify="center">
-      <Select
-        width="200px" 
-        height="50px" 
-        iconColor="black" 
-        backgroundColor="#FF8700"
-        color="black"
-        borderRadius="25px 0px 0px 25px"
-        border="1px solid black"
-        textAlign="center"
-        focusBorderColor="transparent"
-        errorBorderColor="transparent"
-        variant="unstyled"
-      >
-          <option value="" hidden>Filter</option>
-          <option value="a">Author</option>
-          <option value="b">Title</option>
-          <option value="c">Topic</option>
+        <Select
+          width="200px" 
+          height="50px" 
+          iconColor="black" 
+          backgroundColor="#FF8700"
+          color="black"
+          borderRadius="25px 0px 0px 25px"
+          border="1px solid black"
+          textAlign="center"
+          focusBorderColor="transparent"
+          errorBorderColor="transparent"
+          variant="unstyled"
+        >
+            <option value="" hidden>Filter</option>
+            <option value="a">Author</option>
+            <option value="b">Title</option>
+            <option value="c">Topic</option>
         </Select>
         <Flex align="center" position="relative" marginLeft="-1px">
           <Input placeholder="Search..." height="47px" sx={{
             borderRadius: "0px 25px 25px 0px",
-            ':focus': {
+            _focus: {
               borderColor: "#FF8700",
               boxShadow: "0 0 0 1px #FF8700"
             }
@@ -40,10 +44,13 @@ const AddContent: React.FC<AddContentProps> = ({ children }) => {
             <FontAwesomeIcon icon={faSearch} />
           </Box>
         </Flex>
+        <Box marginLeft="1rem">
+          <AddContent trigger={trigger} setTrigger={setTrigger}>{}</AddContent>
+        </Box>
       </Flex>
       {children}
     </Box>
   );
 };
 
-export default AddContent;
+export default SearchEducation;
